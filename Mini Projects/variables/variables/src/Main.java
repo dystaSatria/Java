fc305b4303d6365bdabfe49a390f8f4ebe49a496
REZA DYSTA SATRIA <87264688+dystaSatria@users.noreply.github.com>
2023-07-07 20:20:01 +0300

    Update Main.java

--- Mini Projects/variables/variables/src/Main.java
+++ Mini Projects/variables/variables/src/Main.java
@@ -1,5 +1,4 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+//Find The biggest Number
 public class Main {
     public static void main(String[] args) {
 
