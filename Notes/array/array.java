2412348adcb0e1ebef21b026ab35338421b68784
REZA DYSTA SATRIA <87264688+dystaSatria@users.noreply.github.com>
2023-07-08 15:22:49 +0300

    Update array.java

--- Notes/array/array.java
+++ Notes/array/array.java
@@ -1,1 +1,18 @@
-bdbdsjdhssjd
+class HelloWorld {
+    public static void main(String[] args) {
+        String[] ahha = new String[4];
+        ahha[0] = "Suka";
+        ahha[1] = "duka";
+        
+        for(int i = 0 ; i < ahha.length; i++){
+            System.out.println(ahha[i] + " \n");
+        }
+        
+        //Second Technique
+        
+        for(String a:ahha){
+    System.out.println(a );
+
+        }
+    }
+}
