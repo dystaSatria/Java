0de6adedba8bdd2c89cd9652241b5d7d81d9b278
REZA DYSTA SATRIA <87264688+dystaSatria@users.noreply.github.com>
2023-07-22 21:30:49 +0300

    Add files via upload

---
+++ Mini Projects/Interface/InterfaceSchollTask/Macera.java
@@ -0,0 +1,36 @@
+// Macera.java
+public class Macera implements KitapArayuzu {
+    private String tur;
+    private String yazar;
+    private double fiyat;
+
+    public Macera(String tur, String yazar, double fiyat) {
+        this.tur = tur;
+        this.yazar = yazar;
+        this.fiyat = fiyat;
+    }
+
+    @Override
+    public void turu() {
+        System.out.println("Tür: " + tur);
+    }
+
+    @Override
+    public void yazar() {
+        System.out.println("Yazar: " + yazar);
+    }
+
+    @Override
+    public double fiyat() {
+        return fiyat;
+    }
+
+    @Override
+    public void yazdir() {
+        System.out.println("Kitap Bilgileri:");
+        turu();
+        yazar();
+        System.out.println("Fiyat: " + fiyat());
+        System.out.println();
+    }
+}
